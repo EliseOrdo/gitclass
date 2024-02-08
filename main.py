7eeabcbea2664280c2db5af78f840d522bb34e56
devdl11 <dl11developer@gmail.com>
2024-02-08 08:34:52 +0000

    [feature] add timestone

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ class GemStone:
 		return self.name
 
 
+class TimeStone(GemStone):
+	def __init__(self) -> None:
+		super().__init__()
+		self.name = 'TimeStone'
+
+	def back(amount: int) -> None:
+		print('Rewinding...')
+
+
 if __name__ == '__main__':
 	print('Hello World')
 	# This is the first script of the project
