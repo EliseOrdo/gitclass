19b9446c16ea893b5b850480eeaf79c72f0082f5
devdl11 <dl11developer@gmail.com>
2024-02-08 08:31:07 +0000

    [feature] add basic gemstone

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 
+class GemStone:
+	name: str
+
+	def __init__(self) -> None:
+		self.name = 'random'
+
+	def getName(self) -> str:
+		return self.name
+
 
 if __name__ == '__main__':
 	print('Hello World')
